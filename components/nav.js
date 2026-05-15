a16694067c1f2e36ea91466bf447fc91bd9472fe
Emma Luk <emma07315@gmail.com>
2026-05-14 20:59:11 -0700

    update nav to bring to home page

--- components/nav.js
+++ components/nav.js
@@ -2,7 +2,7 @@ function createNav() {
     const nav = document.createElement('nav');
 
     nav.innerHTML = `
-        <a class="nav-name" href="#">Emma Luk</a>
+        <a class="nav-name" href="../">Emma Luk</a>
         <div class="nav-right">
             <a class="nav-btn" href="https://github.com/emma-luky" target="_blank">GitHub</a>
             <a class="nav-btn" href="https://www.linkedin.com/in/emma-h-luk/" target="_blank">LinkedIn</a>
